e31fc36b038a735b00a3e6bbe8d7b89a35bd9f0d
Kim <a01088570194@gmail.com>
2018-12-26 20:02:12 +0900

    first commit& Add character image

---
+++ background.js
@@ -0,0 +1,18 @@
+
+function click(e) {
+    console.log("start");
+    chrome.tabs.executeScript(null,
+        {
+            code:"document.body.style.backgroundColor='" + e.target.id + "'"
+        }
+    );
+    
+    window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var divs = document.querySelectorAll('div');
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].addEventListener('click', click);
+    }
+});
