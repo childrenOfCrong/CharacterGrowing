2c11ebef105a6b41bc07c9206a0e1ec77c304420
chany <lyhy0310@gmail.com>
2018-12-27 15:46:18 +0900

    add (char create and die)

--- Character.js
+++ Character.js
@@ -1,49 +1,78 @@
 const Character = class {
-  constructor({name, imgUrl="./images/snow.svg"}){
-    this.imgUrl = imgUrl;
-    this.name = name;
-    this._satiety = 100;
-    this._fatigue = 100;
-    this._cleanliness = 100;
-    this._created();
+  constructor(){
+    this.satiety = 100;
+    this.happy = 100;
+    this.clean = 100;
+    this.deerEl = document.querySelector('.deer')
+    this.startTimer();
+    this.move();
+    this.timerIDList = [];
   }
-  _created(){
-   console.log('created');
-    this._render()
+  decreaseSatiety(){
+  
+    const satisEl = document.querySelector('#satiety')
+    const satisTimerID =setInterval(()=>{
+      this.satiety -=30;
+        const staisfies = this.satiety < 0 ? 0 : this.satiety
+      satisEl.style.width = `${staisfies}%`;
+       if(staisfies<=0) this.die()
+       this.timerIDList.push(satisTimerID)
+   }, 10000) 
+   
   }
-  _render(){
-    const char = document.createElement('img')
-    console.log(this.imgUrl)
-    char.setAttribute('src', this.imgUrl)
-    char.setAttribute('class', 'deer')
-    document.querySelector('.backgroundImg').appendChild(char)
+  decreaseHappy(){
+    const happyEl = document.querySelector('#happy')
+     const happyTimerID= setInterval(()=>{
+      this.happy -= 2;
+      const happiness = this.happy < 0 ? 0 : this.happy
+      happyEl.style.width = `${happiness}%`;
+      if(happiness<=0) this.die()
+       this.timerIDList.push(happyTimerID)
+   }, 1000) 
+  
+  }
+  startTimer(){
+    
+    this.decreaseSatiety();
+    this.decreaseHappy();
+    this.poop();
   }
   move(){
-  console.log('created');
+    console.log(this.deerEl)
+    this.deerEl.classList.add('move')
+    console.log('created');
   }
   eat(){
-  console.log('created');
+    console.log('created');
   }
   sleep(){
   console.log('created');
   }
   poop(){
-    console.log('created');
+    const poopTimerID = setInterval(()=>{
+      const shit = document.createElement('img')
+      shit.setAttribute('src', '')
+     document.body.appendChild(shit)
+     this.decreasClean()
+     this.timerIDList.push(poopTimerID)
+   }, 10000) 
+  }
+  decreasClean(){
+    this.clean -=50;
+   const cleanEl = document.querySelector('#clean')
+   cleanEl.style.width = `${this.clean}%`;
+     if(this.clean<=0)this.die()
+  }
+  die(){
+    this.timerIDList.forEach(timerId=>clearInterval(timerId))
+    console.log('die')
   }
 }
-const createCharacter = ()=>{
-  const char = new Character({name: 'name'})
-  console.log('test',char)
- 
-}
+
 
 console.dir(window)
 console.log(window.$_created)
 window.addEventListener('load', ()=>{
-  const created = window.location.search.split('=')[1]
-  console.log(created)
-  if(created==='true'){
-    createCharacter();
-  }
+  const deer = new Character();
 })
 
--- tab.js
+++ tab.js
@@ -10,22 +10,7 @@
 
 
 // chrome.
-window.addEventListener('load', (e)=>{
-  console.log('loaded')
-	const charBtn = document.getElementById('choseCharacter')
-  // charBtn.addEventListener('click', (e)=>{
-  //    window.open(`default.html?char=${true}`);
-	// 	});    
- charBtn.addEventListener('click', (e)=>{
-	  const val = 'character'
-    chrome.storage.sync.set({key: val}, function() {
-			console.log('Value is set to ' + val);
-        });
-		});  
 
- 
-
-})
 
  
 $('.tabular.menu .item').tab();
