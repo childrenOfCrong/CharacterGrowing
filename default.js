7f65cb41752d184d7e24d7104f7ce76b3b014800
chany <lyhy0310@gmail.com>
2018-12-27 09:52:46 +0900

    add: (default chose background)

--- default.js
+++ default.js
@@ -1,6 +1,30 @@
-const imgUrlList = ['./images/bg01.jpg','./images/bg02.png','./images/bg03.png','./images/bg04.jpg','./images/bg05.png']
-  const backgroundImgEl = document.querySelector('.backgroundImg')
+const imgUrlList = ['../images/bg01.jpg','../images/bg02.png','../images/bg03.png','../images/bg04.jpg','../images/bg05.png'];
+const backgroundImgEl = document.querySelector('.backgroundImg');
+const snowEl = document.querySelector('.snow');
+const tableEl = document.querySelector('.table');
   
-  const getRandomIdx = num=>Math.floor(num*Math.random())
+const hideEl = (el)=>el.classList.add('hide')
+const showEl = (el)=>el.classList.remove('hide')
+const makeDisplayNoneEl = (el)=>el.classList.add('none')  
 
-  backgroundImgEl.setAttribute('src',imgUrlList[getRandomIdx(imgUrlList.length)]); 
+const showChosedBg = (id)=>{
+  const bgUrl = imgUrlList[id];
+  backgroundImgEl.style.backgroundImage =`url(${bgUrl})`
+  if(id===4) backgroundImgEl.classList.add('lastBg')
+  else backgroundImgEl.classList.remove('lastBg')
+  
+  showEl(backgroundImgEl);
+
+}
+
+
+
+tableEl.addEventListener('click', (e)=>{
+ const bgItem = e.target.closest('[data-cell]')
+ if(!bgItem) return;
+ const bgID = +(bgItem.dataset.cell);
+ makeDisplayNoneEl(tableEl);
+ showChosedBg(bgID);
+ showEl(snowEl);
+})
+
