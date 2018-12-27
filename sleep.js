f7ba0401eb0dedc9d23dd8b9fb3a5379db01e6d7
cohily12 <k96079123@gmail.com>
2018-12-27 16:04:50 +0900

    [Add]sleeping mode

---
+++ sleep.js
@@ -0,0 +1,7 @@
+const deer = document.querySelector("#deer");
+const sleepBtn = document.querySelector("#sleep");
+
+sleepBtn.addEventListener("click", function(){
+    console.log("sleep");
+    
+})
